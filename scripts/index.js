9ce6ee1009fc5bc47f03715584ac6ea92c0a2cca
AnurupMondal <anurup.mondal75@gmail.com>
2023-08-03 01:20:50 +0530

    reviews made

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+var typed = new Typed('#typed', {
+    strings: [
+        `<h1 class="bio">Hi, I'm <span class="highlight">Anurup</span></h1> <h1 class="bio">Chandra Mondal from India</h1>`
+    ],
+    typeSpeed: 50,
+    // backSpeed: 50,
+    // loop: true,
+    // loopCount: Infinity,
+    showCursor: true,
+    cursorChar: '',
+    autoInsertCss: true,
+    attr: null,
+    contentType: 'html',
+});
